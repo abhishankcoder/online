82b20351437089c3fc7fee4b9a8c07ac34a1e8f0
abhishankcoder <chaturvediabhishank90@gmail.com>
2022-06-25 12:38:34 +0530

    ready for deploye

--- demo/settings.py
+++ demo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,4 +145,4 @@ REST_FRAMEWORK = {
 }
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 MEDIA_URL= "/media/"
- 
+django_heroku.settings(locals())
